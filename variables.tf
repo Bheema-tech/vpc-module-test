b05b8e9733579597b72e45df526790d004185145
bheema-tech <bheemesha590@gmail.com>
2025-06-28 12:31:14 +0530

    Terraform-basics

--- variables.tf
+++ variables.tf
@@ -4,3 +4,15 @@ variable "project" {
 variable "environment" {
     default = "dev" 
 }
+
+variable "public_cidr_block" {
+     default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_cidr_block" {
+     default = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "database_cidr_block" {
+     default = ["10.0.23.0/24", "10.0.24.0/24"]
+}
